f7d37c76ecd64ed3b54ce00656bca5f14e2d9f74
Carlos C Soto <eclipxe13@gmail.com>
2025-08-21 11:07:58 -0600

    Fix RuleSet for PHP minor versions

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -15,8 +15,8 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
         '@PSR12:risky' => true,
-        '@PHP71Migration:risky' => true,
-        '@PHP73Migration' => true,
+        '@PHP82Migration:risky' => true,
+        '@PHP82Migration' => true,
         // symfony
         'class_attributes_separation' => true,
         'whitespace_after_comma_in_array' => true,
